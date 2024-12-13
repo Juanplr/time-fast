edfb5b3b35eab3edb103a1c20bc106899d80139d
unknown <dangja9@gmail.com>
2024-12-12 22:23:17 -0600

    paquetes

--- API time-fast/src/java/ws/ApplicationConfig.java
+++ API time-fast/src/java/ws/ApplicationConfig.java
@@ -31,5 +31,6 @@ public class ApplicationConfig extends Application {
      private void addRestResourceClasses(Set<Class<?>> resources) {
          resources.add(ws.WSColaborador.class);
         resources.add(ws.WSLogin.class);
+        resources.add(ws.WSPaquete.class);
      }
 }
