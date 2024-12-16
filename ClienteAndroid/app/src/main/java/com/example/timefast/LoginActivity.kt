28e904e78d28f51076c0aa9a7a453159493a03ea
Frantatita <reyes.23.jacome@gmail.com>
2024-12-16 03:17:21 -0600

    clienteAndroid

--- ClienteAndroid/app/src/main/java/com/example/timefast/LoginActivity.kt
+++ ClienteAndroid/app/src/main/java/com/example/timefast/LoginActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.timefast.databinding.ActivityLoginBinding
+import com.example.timefast.poko.Colaborador
 import com.example.timefast.poko.LoginColaborador
 import com.example.timefast.util.Constantes
 import com.google.gson.Gson
@@ -56,7 +57,7 @@ class LoginActivity : AppCompatActivity() {
     fun verificarCredenciales(noPersonal: String, contrasena: String ){
         Ion.getDefault(this@LoginActivity).conscryptMiddleware.enable(false)
         Ion.with(this@LoginActivity)
-            .load("POST","${Constantes().url_ws}/login/iniciar-sesion")
+            .load("POST","${Constantes().url_ws}/login/iniciar-sesion-app")
             .setHeader("Content-Type","application/x-www-form-urlencoded")
             .setBodyParameter("noPersonal",noPersonal)
             .setBodyParameter("contrasena",contrasena)
@@ -66,15 +67,12 @@ class LoginActivity : AppCompatActivity() {
                     Log.d("LoginActivity","Respuesta del servidor: $result" )
                     if(result != null && result.startsWith("{")){
                         try{
-                            Log.d("LoginActivity","Entro al metodo para realiazr la serializacion")
                             serializarInformacion(result)
                         }catch (ex:JsonSyntaxException){
                             Log.e("LoginActivity", "Error de deserialización: ${ex.message}")
-                            Toast.makeText(this@LoginActivity, "Error al procesar la respuesta del servidor", Toast.LENGTH_LONG).show()
                         }
-                    }else {
-                        Log.d("LoginActivity", "No entro al metodo para realiazr la serializacion")
-                        Toast.makeText(this@LoginActivity, "Respuesta inesperada del servidor", Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(this@LoginActivity, "Numero Personal o Contraseña incorrectos", Toast.LENGTH_LONG).show()
                     }
                 }else{
                     Toast.makeText(this@LoginActivity, "Error: ${e.message}", Toast.LENGTH_LONG).show()
@@ -85,18 +83,20 @@ class LoginActivity : AppCompatActivity() {
     public fun serializarInformacion(json: String){
         try{
             val gson = Gson()
-            val respuestaLoginColaborador = gson.fromJson(json, LoginColaborador::class.java)
-            Log.d("LoginActivity", "Cliente deserializado: ${respuestaLoginColaborador.Colaborador}")
-            if(respuestaLoginColaborador.Colaborador!=null){
-                val clienteJSON = gson.toJson(respuestaLoginColaborador.Colaborador);
+            val respuestaLoginColaborador = gson.fromJson(json, Colaborador::class.java)
+            Log.d("LoginActivity", "Cliente deserializado: ${respuestaLoginColaborador}")
+            if(respuestaLoginColaborador!=null){
+                val clienteJSON = gson.toJson(respuestaLoginColaborador);
                 Log.d("LoginActivity",clienteJSON)
                 irPantallaPrincipal(clienteJSON);
+            }else {
+                Log.d("LoginActivity", "El campo Colaborador está vacío o es nulo")
+                Toast.makeText(this@LoginActivity, "Inicio de sesión fallido, datos inválidos", Toast.LENGTH_LONG).show()
             }
         }catch(ex: JsonSyntaxException){
             Log.e("LoginActivity", "Error de deserialización: ${ex.message}")
             Toast.makeText(this@LoginActivity, "Error al procesar la respuesta del servidor", Toast.LENGTH_LONG).show()
         }
-
     }
 
     fun irPantallaPrincipal(colaborador:String){
